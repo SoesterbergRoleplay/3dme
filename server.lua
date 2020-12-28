749fde2ec34721f3bf25b000423ca2a2b55c35db
SoesterbergRoleplay <74326178+SoesterbergRoleplay@users.noreply.github.com>
2020-12-28 09:24:33 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,6 +2,6 @@
 
 -- Command
 RegisterCommand('me', function(source, args)
-    local text = "* " .. Languages[Config.language].prefix .. table.concat(args, " ") .. " *"
+    local text = "" .. Languages[Config.language].prefix .. table.concat(args, " ") .. ""
     TriggerClientEvent('3dme:shareDisplay', -1, text, source)
 end)
